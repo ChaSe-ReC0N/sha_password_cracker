7965b7e42bd674105ba86beba4b973b7837da37c
Akshay B <77104366+ChaSe-ReC0N@users.noreply.github.com>
2022-06-27 21:43:07 +0530

    Update sha1-password-cracker-main.py

--- sha1-password-cracker-main.py
+++ sha1-password-cracker-main.py
@@ -1,17 +1,31 @@
 import hashlib as hlib
 
-hs=input("Input the sha_hash to be cracked")
-pass_file = input("Input the filename of password/Wordlist")
-#pass_list =
-
-def sha_1_cracker():
-    hash = hs
-    
-    
+def sha_1_cracker(hs):
+    hash = hs  
 
 def sha_1_gen(hs):
     sha = hlib.sha1(str(hs).encode("utf-8")).hexdigest()  
     return sha 
 
+ch = input("1. to generate a hash\n2.to crack password")
 
-print("Generated sha1 hash = ",sha_1_gen(hs))
+if (ch == "1"):#hash generation
+    hs=input("Input the string to be hashed\n")    
+    print("Generated sha1 hash = ",sha_1_gen(hs))
+
+elif(ch=="2"):
+    hs=input("Input the hash to be cracked\n")
+    stri = input("Enter the string\n")
+    #pass_file = input("Input the filename of password/Wordlist")
+    #pass_list =
+    sha_ge = sha_1_gen(stri)
+
+    if(sha_ge== hs):
+        print (f"password is = {stri}")
+
+    else:
+        print("Wrong password")
+ 
+else:
+    print("Wrong Choice")
+    
